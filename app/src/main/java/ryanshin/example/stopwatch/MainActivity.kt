ceedcb11792d7b01aa54f283e9b392f2f96f315c
Ryan <ryan.shin@kakaocorp.com>
2022-11-27 13:32:14 +0900

    StopWatch Finish"
    ;

--- app/src/main/java/ryanshin/example/stopwatch/MainActivity.kt
+++ app/src/main/java/ryanshin/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package ryanshin.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import java.util.*
 import ryanshin.example.stopwatch.databinding.ActivityMainBinding
 import kotlin.concurrent.timer
@@ -10,17 +11,27 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var time = 0
     private var timerTask: Timer? = null
-    private  var isRunning = false
+    private var isRunning = false
+    private var lap = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //시작버튼눌렀을때 액션
         binding.fab.setOnClickListener {
             isRunning = !isRunning
             if ( isRunning)
                 start()
             else
                 pause()
+        }
+        // lapButton 눌렀을때 액션
+        binding.labButton.setOnClickListener {
+            recordLapTime()
+        }
+        binding.resetFab.setOnClickListener {
+            reset()
         }
     }
     private fun start(){
@@ -38,5 +49,26 @@ class MainActivity : AppCompatActivity() {
     private fun pause(){
         binding.fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
         timerTask?.cancel()
+    }
+    private fun recordLapTime(){
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap LAB : ${lapTime / 100}.${lapTime % 100}"
+
+        //맨위에 랩타임 추가
+        binding.labLayout.addView(textView, 0)
+        lap++
+    }
+    private fun reset(){
+        timerTask?.cancel()
+        //모든 변수 초기화
+        this.time = 0
+        this.isRunning = false
+        binding.fab.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+        binding.secTextView.text = "0"
+        binding.milliTextView.text = "00"
+        // 모든 랩타임 제거
+        binding.labLayout.removeAllViews()
+        this.lap = 1
     }
 }
